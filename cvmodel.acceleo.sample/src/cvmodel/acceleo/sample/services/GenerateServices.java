c3999776d69baa90c60a65d5db616c9c33b656dd
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-11-22 00:16:42 +0000

    Fixed dependency things.

--- cvmodel.acceleo.sample/src/cvmodel/acceleo/sample/services/GenerateServices.java
+++ cvmodel.acceleo.sample/src/cvmodel/acceleo/sample/services/GenerateServices.java
@@ -1,7 +1,29 @@
 package cvmodel.acceleo.sample.services;
 
+import cv.Person;
+import cv.PhoneCountryCode;
+import cv.StartEndTimeFrame;
+import cv.TimeFrame;
+
+import java.text.SimpleDateFormat;
+
 public class GenerateServices {
-	public String formatDate5() {
-		return "bazinga";
+	static final SimpleDateFormat sdf = new SimpleDateFormat("MMM, yyy"); 
+	static final SimpleDateFormat birthdaysdf = new SimpleDateFormat("MMM dd, yyyy"); 
+	
+	public static String formatDate(TimeFrame tf) {
+		return sdf.format(tf.getStartDate()) + " - now";
+	}
+	
+	public static String formatDateStartEnd(StartEndTimeFrame setf) {
+		return sdf.format(setf.getStartDate()) + " - " + sdf.format(setf.getEndDate());
+	}
+	
+	public static String formatBirthDate(Person p) {
+		return birthdaysdf.format(p.getDateOfBirth());
+	}
+	
+	public static Integer getCountryCodeExtension(PhoneCountryCode countryCode) {
+		return countryCode.getValue();
 	}
 }
